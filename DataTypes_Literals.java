a0a0e661385ca23d610fe9d9b67d586f48f47661
Shlok Arora <128911480+ShlokArora2709@users.noreply.github.com>
2024-03-29 13:28:57 +0000

    data types done and few literals

---
+++ DataTypes_Literals.java
@@ -0,0 +1,43 @@
+public class DataTypes_Literals {
+    public static void main(String[] args){
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+
+        
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+
+
+        //Literals 
+        
+        int dec=14;             //Decimal- Int basically        
+        int oct= 0227;          //Octal- Sarts with 0
+        int hex= 0x75ACE;       //Hex - 0x/0X
+        int bina=0b101110;      //binary -0b/0B
+        //char string and a few more
+
+        System.out.println(dec);
+        System.out.println(oct);
+        System.out.println(hex);
+        System.out.println(bina);
+
+
+
+    }
+}
+/*
+ * byte - 1 byte
+ * short - 2
+ * int - 4
+ * long - 8
+ * float - 4
+ * double - 8
+ * bool- 1
+ * char - 2 ASCII
+ */
