4bb6ce5f5e6790eadddedfe00933af045e08ca7b
Shlok Arora <shlokarora2709@gmail.com>
2024-05-01 13:06:38 +0530

    Refactor AWT code to use Association instead of Inheritance

--- java_AWT.java
+++ java_AWT.java
@@ -1,4 +1,8 @@
 import java.awt.*;
+
+/* 
+// AWT by inheritance of Frame class
+
 public class java_AWT extends Frame{
     java_AWT(){
         Button b=new Button("click me");
@@ -8,6 +12,53 @@ public class java_AWT extends Frame{
         setTitle("AWT window");
         setLayout(null);
         setVisible(true);
+    }
+    public static void main(String[] args) {
+        new java_AWT();
+    }
+    
+}
+*/
+
+//AWT by Association:
+public class java_AWT{
+    java_AWT(){
+        Frame f=new Frame();
+        Button b=new Button("Submit");
+        Label l=new Label("Employees");
+        TextField t=new TextField();
+        Checkbox c= new Checkbox("Python",true);
+        Choice ch=new Choice();
+        List l1=new List(5);
+
+        b.setBounds(20,100,80,30);
+        l.setBounds(20,80,80,30);
+        t.setBounds(100,100,80,30);
+        c.setBounds(100,80,80,30);
+        ch.setBounds(100, 50, 80, 30);
+        l1.setBounds(100, 150, 80, 80);
+
+        ch.add("Java");
+        ch.add("Python");
+        ch.add("C++");
+        
+        l1.add("Java");
+        l1.add("Python");
+        l1.add("C++");
+        l1.add("C#");
+        l1.add("Ruby");
+
+        f.add(b);
+        f.add(l);
+        f.add(t);
+        f.add(c);
+        f.add(ch);
+        f.add(l1);
+
+        f.setSize(400,300);
+        f.setTitle("AWT window");
+        f.setLayout(null);
+        f.setVisible(true);
     }
     public static void main(String[] args) {
         new java_AWT();
